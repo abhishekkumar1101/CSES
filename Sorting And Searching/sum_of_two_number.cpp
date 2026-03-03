aab115640fa98d390b4787eab880af681172fb51
abhishekkumar1101 <pal32162@gmail.com>
2026-03-03 10:29:44 +0530

    second day

---
+++ Sorting And Searching/sum_of_two_number.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+//#define for(i, n) for (int i = 0; i < n; i++)
+//#define for(i, k, n) for (int i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, false, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+vector<int> SieveOfEratosthenes(int n,vector<int>&pnum){
+vector<bool> prime(n + 1, true);
+for (int p = 2; p * p <= n; p++) {
+if (prime[p] == true) {
+for (int i = p * p; i <= n; i += p)
+prime[i] = false;
+}
+}
+for (int p = 2; p <= n; p++){
+if (prime[p]) pnum.push_back(p);
+}
+return pnum;
+} 
+ 
+void solve(){
+ int n,x;
+cin>>n>>x;
+int arr[n];
+map<int,vector<int>> mp;
+for(int i =0;i<n;i++) cin>>arr[i],mp[arr[i]].pb(i+1);
+sort(arr,arr+n);
+int i = 0,j=n-1;
+while(i<j){
+    if(arr[i]+arr[j]>x){
+        j--;
+    }
+    else if(arr[i]+arr[j]<x){
+        i++;
+    }
+    else{
+        cout<<mp[arr[i]].back()<<" ";
+        mp[arr[i]].pop_back();
+        cout<<mp[arr[j]].back();
+        return;
+    }
+}
+cout<<"IMPOSSIBLE\n";
+}
+ 
+ 
+int main(){
+ios_base::sync_with_stdio(0);
+cin.tie(0);
+cout.tie(0);
+int t=1;
+
+while (t--){
+solve();
+}
+return 0;
+}
